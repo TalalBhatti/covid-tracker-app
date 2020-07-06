0a3438daf4805d5c1c644608efdff920d183621c
TalalBhatti <talalabhatti@gmail.com>
2020-07-06 16:46:35 +0500

    Adding files

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Header.css';
+
+const Header = (props) => {
+    return (
+        <div className="heading">
+        <h1>COVID-19 TRACKER APP</h1>
+        </div>
+    )
+}
+
+export default Header;
